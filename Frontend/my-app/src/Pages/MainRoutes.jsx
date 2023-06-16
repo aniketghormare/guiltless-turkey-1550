060d8d9794d854adab4157376705c06f5c8476b7
prajit96 <santraprajit@gmail.com>
2023-06-16 23:36:07 +0530

    navbar done

--- Frontend/my-app/src/Pages/MainRoutes.jsx
+++ Frontend/my-app/src/Pages/MainRoutes.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import {Games} from './Games';
 import {Movies} from './Movies'
+import Navbar from '../Components/Navbar';
 
 export const MainRoutes = () => {
   return <Routes>
-    {/* <Route path='/navbar' element={<Navbar/>}/> */}
+    <Route path='/navbar' element={<Navbar/>}/>
     <Route path='/games' element={<Games/>}/>
     <Route path='/movies' element={<Movies/>}/>
   </Routes>
